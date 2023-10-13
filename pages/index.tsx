56d9f9672784e58684d4c1248c7c9b9c4cbc41fc
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2023-10-13 15:44:20 +0530

    Remove junk + initialize /pages /components

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Root - Automated trading strategies for providing liquidity on Solana</title>
+        <link rel="icon" href="/images/root-logo.png" />
+      </Head>
+      Home page here
+    </div>
+  );
+}
