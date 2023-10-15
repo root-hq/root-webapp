fa5050d6986d9f757fba475a7afa55981502242c
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2023-10-15 06:50:22 +0530

    Add returning dummy book on error

--- utils/phoenix/orderbook.ts
+++ utils/phoenix/orderbook.ts
@@ -1,13 +1,44 @@
-import { Client, L3UiBook } from "@ellipsis-labs/phoenix-sdk";
+import { Client, L3UiBook, Side } from "@ellipsis-labs/phoenix-sdk";
 import { web3 } from "@coral-xyz/anchor";
 
 export const getL3Book = async(
     marketAddress: string,
     depthPerSide: number
 ): Promise<L3UiBook> => {
-    const connection = new web3.Connection(process.env.NEXT_RPC_ENDPOINT, { commitment: 'processed' });
+    try {
+        const connection = new web3.Connection(process.env.NEXT_RPC_ENDPOINT, { commitment: 'processed' });
     
-    const client = await Client.create(connection);
-    const book = client.getL3UiBook(marketAddress, depthPerSide);
-    return book;
+        const client = await Client.create(connection);
+        const book = client.getL3UiBook(marketAddress, depthPerSide);
+        return book;
+    }
+    catch(err) {
+        console.log('Error fetching orderbook data on Pheonix: ', err);
+    }
+
+    return {
+        bids: [
+            {
+                price: 0,
+                side: Side.Bid,
+                size: 0,
+                makerPubkey: '',
+                orderSequenceNumber: '',
+                lastValidSlot: 0,
+                lastValidUnixTimestampInSeconds: 0
+            }
+        ],
+        asks: [
+            {
+                price: 0,
+                side: Side.Ask,
+                size: 0,
+                makerPubkey: '',
+                orderSequenceNumber: '',
+                lastValidSlot: 0,
+                lastValidUnixTimestampInSeconds: 0
+            }
+        ]
+
+    }
 }
