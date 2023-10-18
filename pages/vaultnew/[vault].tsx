a60f33501ad85297f4ef282a4825cb30e44aeaab
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2023-10-18 19:25:02 +0530

    Add error msg for invalid vault

--- pages/vaultnew/[vault].tsx
+++ pages/vaultnew/[vault].tsx
@@ -28,47 +28,57 @@ const NewVaultPage = ({ vaultData, baseTokenMetadata, quoteTokenMetadata }: NewV
     }, []);
 
     return(
-        <Container className={styles.vaultPageContainer}>
-            <Row className={styles.vaultInfoContainer}>
-                <TokenImageContainer 
-                    baseTokenImageMetadata={
-                        {
-                            url: baseTokenMetadata.img_url,
-                            width: windowSize[0] > 425 ? 45 : 35,
-                            height: windowSize[0] > 425 ? 45 : 35,
-                            alt: `Base token: ${baseTokenMetadata.ticker}`
-                        } as ImageMetadata
-                    }
-                    quoteTokenImageMetadata={
-                        {
-                            url: quoteTokenMetadata.img_url,
-                            width: windowSize[0] > 425 ? 45 : 35,
-                            height: windowSize[0] > 425 ? 45 : 35,
-                            alt: `Quote token: ${quoteTokenMetadata.ticker}`
-                        } as ImageMetadata
-                    }
-                />
-                <span className={styles.vaultPairNameContainer}>{`${baseTokenMetadata.ticker}`}</span>
-                <span className={styles.vaultPairNameContainer}>{`${quoteTokenMetadata.ticker}`}</span>
-            </Row>
-            <Row><PairInfoRow /></Row>
-            <Row><VaultInfoRow /></Row>
-        </Container>
-    )
+        
+            (vaultData && baseTokenMetadata && quoteTokenMetadata) ?
+                <Container className={styles.vaultPageContainer}>
+                    <Row className={styles.vaultInfoContainer}>
+                        <TokenImageContainer 
+                            baseTokenImageMetadata={
+                                {
+                                    url: baseTokenMetadata.img_url,
+                                    width: windowSize[0] > 425 ? 45 : 35,
+                                    height: windowSize[0] > 425 ? 45 : 35,
+                                    alt: `Base token: ${baseTokenMetadata.ticker}`
+                                } as ImageMetadata
+                            }
+                            quoteTokenImageMetadata={
+                                {
+                                    url: quoteTokenMetadata.img_url,
+                                    width: windowSize[0] > 425 ? 45 : 35,
+                                    height: windowSize[0] > 425 ? 45 : 35,
+                                    alt: `Quote token: ${quoteTokenMetadata.ticker}`
+                                } as ImageMetadata
+                            }
+                        />
+                        <span className={styles.vaultPairNameContainer}>{`${baseTokenMetadata.ticker}`}</span>
+                        <span className={styles.vaultPairNameContainer}>{`${quoteTokenMetadata.ticker}`}</span>
+                    </Row>
+                    <Row><PairInfoRow /></Row>
+                    <Row><VaultInfoRow /></Row>
+                </Container>
+            :
+                <span className={styles.vaultNotFound}>
+                    Could not find relevant vault
+                </span>  
+    );
 }
 
 export async function getServerSideProps({ params }) {
     const { vault } = params;
     
     const vaultData = await getVault(vault);
+    let baseTokenMetadata = null;
+    let quoteTokenMetadata = null;
     
-    const [
-        baseTokenMetadata,
-        quoteTokenMetadata,
-    ] = await Promise.all([
-        getTokenMetadata(vaultData.base_token_address),
-        getTokenMetadata(vaultData.quote_token_address),
-    ]);
+    if(vaultData && vaultData.base_token_address && vaultData.quote_token_address) {
+        [
+            baseTokenMetadata,
+            quoteTokenMetadata,
+        ] = await Promise.all([
+            getTokenMetadata(vaultData.base_token_address),
+            getTokenMetadata(vaultData.quote_token_address),
+        ]);
+    }
 
     return {
         props: {
