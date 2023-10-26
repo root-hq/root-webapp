208cf54f3927773446ff80f606dbe5316c07731e
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2023-10-26 12:59:17 +0530

    Remove vault and switch to vaultnew

--- pages/vault/[vault].tsx
+++ pages/vault/[vault].tsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
-import styles from "./NewVaultPage.module.css";
-import { Col, Container, Row } from "react-bootstrap";
-import PairInfoRow from "./components/PairInfoRow";
-import VaultInfoRow from "./components/VaultInfoRow";
+import styles from "./VaultPage.module.css";
 import {
   TokenMetadata,
+  TokenPrice,
   UnifiedVault,
   getTokenMetadata,
   getVault,
 } from "../../utils/supabase";
+import Vault from "./components/Vault/Vault";
+import User from "./components/User/User";
 import { VaultBalance, getVaultBalance } from "../../utils/root/utils";
 import { getTokenPrice } from "../../utils/token";
+import { getTokenPriceDataWithDate } from "../../utils/supabase/tokenPrice";
+import { getL3Book, getMarketMidPrice } from "../../utils/phoenix";
+import { L3UiBook } from "@ellipsis-labs/phoenix-sdk";
+import { DEFAULT_ORDERBOOK_VIEW_DEPTH } from "../../constants";
 
-export interface NewVaultPageProps {
+export interface VaultPageProps {
   vaultData: UnifiedVault;
+  l3UiBook: L3UiBook;
   baseTokenMetadata: TokenMetadata;
   quoteTokenMetadata: TokenMetadata;
   baseTokenPrice: number;
@@ -22,15 +27,16 @@ export interface NewVaultPageProps {
   quoteTokenBalance: number;
 }
 
-const NewVaultPage = ({
+const VaultPage = ({
   vaultData,
+  l3UiBook,
   baseTokenMetadata,
   quoteTokenMetadata,
   baseTokenPrice,
   baseTokenBalance,
   quoteTokenPrice,
   quoteTokenBalance,
-}: NewVaultPageProps) => {
+}: VaultPageProps) => {
   const [windowSize, setWindowSize] = useState([0, 0]);
 
   // UseEffect to handle windowSizing for custom CSS
@@ -47,37 +53,110 @@ const NewVaultPage = ({
     };
   }, []);
 
-  return vaultData && baseTokenMetadata && quoteTokenMetadata ? (
-    <Container className={styles.vaultPageContainer}>
-      <Row className={styles.highLevelPairInfoContainer}>
-        <Col className={styles.lowLevelPairInfoContainer}>
-          <PairInfoRow
-            vaultData={vaultData}
-            baseTokenMetadata={baseTokenMetadata}
-            quoteTokenMetadata={quoteTokenMetadata}
-            baseTokenBalance={baseTokenBalance}
-            quoteTokenBalance={quoteTokenBalance}
-            baseTokenDepositsValue={baseTokenBalance * baseTokenPrice}
-            quoteTokenDepositsValue={quoteTokenBalance * quoteTokenPrice}
-            tokenImgWidth={windowSize[0] > 425 ? 40 : 35}
-            tokenImgHeight={windowSize[0] > 425 ? 40 : 35}
-          />
-        </Col>
-      </Row>
-      <Row className={styles.highLevelVaultInfoContainer}>
-        <VaultInfoRow
-          vaultData={vaultData}
-          baseTokenMetadata={baseTokenMetadata}
-          quoteTokenMetadata={quoteTokenMetadata}
-          baseTokenPrice={baseTokenPrice}
-          baseTokenBalance={baseTokenBalance}
-          quoteTokenPrice={quoteTokenPrice}
-          quoteTokenBalance={quoteTokenBalance}
-        />
-      </Row>
-    </Container>
-  ) : (
-    <span className={styles.vaultNotFound}>Could not find relevant vault</span>
+  const [priceSeries, setPriceSeries] = useState([] as number[][]);
+
+  const [l3UiBookState, setL3UiBookState] = useState({
+    bids: [],
+    asks: [],
+  });
+  const [newPrice, setNewPrice] = useState(0);
+  const [previousPrice, setPreviousPrice] = useState(0);
+  const [midPriceChangeDirection, setMidPriceChangeDirection] = useState("");
+
+  useEffect(() => {
+    const refreshPriceData = async () => {
+      try {
+        const today = new Date();
+        const newMidPrice = parseFloat(
+          (await getMarketMidPrice(vaultData.market_address)).toFixed(3),
+        );
+
+        setNewPrice((prevPrice) => newMidPrice);
+
+        if (newMidPrice >= previousPrice) {
+          setMidPriceChangeDirection((previousChange) => "▲");
+        } else {
+          setMidPriceChangeDirection((previousChange) => "▼");
+        }
+
+        setPreviousPrice((prevPrice2) => newMidPrice);
+        if (priceSeries.length === 0) {
+          const freshPrices = (
+            await getTokenPriceDataWithDate(vaultData.market_address, today)
+          ).map((val: TokenPrice) => [val.timestamp, val.price]);
+
+          setPriceSeries((prevPrices) => [...freshPrices]);
+        } else {
+          setPriceSeries((prevPrices) => [
+            ...prevPrices.slice(1),
+            [Date.now(), newMidPrice],
+          ]);
+        }
+      } catch (error) {
+        console.error("Error fetching price data:", error);
+      }
+    };
+
+    // Fetch price data initially
+    refreshPriceData();
+
+    // Set up an interval to fetch price data every 5 seconds (adjust as needed)
+    const intervalId = setInterval(() => {
+      refreshPriceData();
+    }, 2000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [previousPrice]); // Empty dependency array to run the effect only once on mount
+
+  useEffect(() => {
+    const refreshBook = async () => {
+      const newBook = await getL3Book(
+        vaultData.market_address,
+        DEFAULT_ORDERBOOK_VIEW_DEPTH,
+      );
+      setL3UiBookState((prevBook) => newBook);
+    };
+
+    refreshBook();
+
+    const intervalId = setInterval(() => {
+      refreshBook();
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [vaultData.market_address]);
+
+  return (
+    <div className={styles.vaultPageContainer}>
+      {vaultData ? (
+        <>
+          <div className={styles.vaultDataContainer}>
+            <Vault
+              vaultData={vaultData}
+              baseTokenMetadata={baseTokenMetadata}
+              quoteTokenMetadata={quoteTokenMetadata}
+              midPrice={previousPrice}
+              priceSeries={priceSeries}
+              l3UiBook={l3UiBookState}
+              priceChangeDirection={midPriceChangeDirection}
+              tokenImgWidth={windowSize[0] > 425 ? 40 : 35}
+              tokenImgHeight={windowSize[0] > 425 ? 40 : 35}
+            />
+          </div>
+          <div className={styles.userDataContainer}>
+            <User
+              baseTokenMetadata={baseTokenMetadata}
+              baseTokenBalance={baseTokenBalance}
+              quoteTokenMetadata={quoteTokenMetadata}
+              quoteTokenBalance={quoteTokenBalance}
+            />
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 };
 
@@ -87,6 +166,10 @@ export async function getServerSideProps({ params }) {
   const vaultData = await getVault(vault);
   let baseTokenMetadata: TokenMetadata = null;
   let quoteTokenMetadata: TokenMetadata = null;
+  let l3UiBook = {
+    bids: [],
+    asks: [],
+  } as L3UiBook;
   let vaultBalance: VaultBalance = null;
   let baseTokenPrice: number = null;
   let quoteTokenPrice: number = null;
@@ -96,11 +179,13 @@ export async function getServerSideProps({ params }) {
     vaultData.base_token_address &&
     vaultData.quote_token_address
   ) {
-    [baseTokenMetadata, quoteTokenMetadata, vaultBalance] = await Promise.all([
-      getTokenMetadata(vaultData.base_token_address),
-      getTokenMetadata(vaultData.quote_token_address),
-      getVaultBalance(vault),
-    ]);
+    [baseTokenMetadata, quoteTokenMetadata, l3UiBook, vaultBalance] =
+      await Promise.all([
+        getTokenMetadata(vaultData.base_token_address),
+        getTokenMetadata(vaultData.quote_token_address),
+        getL3Book(vaultData.market_address, DEFAULT_ORDERBOOK_VIEW_DEPTH),
+        getVaultBalance(vault),
+      ]);
 
     if (baseTokenMetadata && baseTokenMetadata.ticker) {
       baseTokenPrice = await getTokenPrice(baseTokenMetadata.ticker, "USD");
@@ -116,6 +201,7 @@ export async function getServerSideProps({ params }) {
       vaultData: vaultData ? vaultData : null,
       baseTokenMetadata: baseTokenMetadata ? baseTokenMetadata : null,
       quoteTokenMetadata: quoteTokenMetadata ? quoteTokenMetadata : null,
+      l3UiBook: l3UiBook ? l3UiBook : null,
       baseTokenPrice: baseTokenPrice ? baseTokenPrice : null,
       baseTokenBalance: vaultBalance.baseTokenBalance
         ? vaultBalance.baseTokenBalance
@@ -128,4 +214,4 @@ export async function getServerSideProps({ params }) {
   };
 }
 
-export default NewVaultPage;
+export default VaultPage;
