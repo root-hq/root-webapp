0e0d2d109fd5747a4a09676794debd9989d97f57
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-01-26 15:24:34 +0530

    Add user quoteTokenBalance

--- pages/market/components/OrderProducer/index.tsx
+++ pages/market/components/OrderProducer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./OrderProducer.module.css";
 import { SpotGridMarket, TokenMetadata } from "../../../../utils/supabase";
 import { Button, Form } from "react-bootstrap";
@@ -10,6 +10,9 @@ const WalletMultiButtonDynamic = dynamic(
     (await import("../../../../components/Wallet")).WalletMultiButton,
   { ssr: false },
 );
+import { Connection } from "@solana/web3.js";
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import { web3 } from "@coral-xyz/anchor";
 
 export interface OrderProducerProps {
   spotGridMarket: SpotGridMarket;
@@ -30,20 +33,50 @@ const OrderProducer = ({
   const [maxPrice, setMaxPrice] = useState("");
   const [numGrids, setNumGrids] = useState("");
   const [amountQuote, setAmountQuote] = useState("");
+  const [baseTokenBalance, setBaseTokenBalance] = useState(0.0);
+  const [quoteTokenBalance, setQuoteTokenBalance] = useState(0.0);
 
-  const removeCommas = (value) => {
-    return value.replace(/,/g, "");
-  };
+  let connection: Connection;
+  if(process.env.RPC_ENDPOINT) {
+    connection = new Connection(process.env.RPC_ENDPOINT, { commitment: "processed" });
+  }
+  else {
+    connection = new Connection(`https://api.mainnet-beta.solana.com/`, { commitment: "processed" });
+  }
 
-  const formatWithCommas = (value) => {
-    const withoutCommas = removeCommas(value);
+  useEffect(() => {
+    const updateBalance = async() => {
+      if(walletState.connected) {
+        const baseTokenAddress = await getAssociatedTokenAddress(new web3.PublicKey(baseTokenMetadata.mint), walletState.publicKey);
+        const quoteTokenAddress = await getAssociatedTokenAddress(new web3.PublicKey(quoteTokenMetadata.mint), walletState.publicKey);
 
-    const parts = withoutCommas.split(".");
-    const integerPart = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    const formattedValue =
-      parts.length > 1 ? `${integerPart}.${parts[1]}` : integerPart;
-    return formattedValue;
-  };
+        console.log("Base token address: ", baseTokenAddress.toString());
+        console.log("Quote token address: ", quoteTokenAddress.toString());
+
+        let baseBalance = 0;
+        if((await connection.getBalance(baseTokenAddress)) > 0) {
+          baseBalance = (await connection.getTokenAccountBalance(baseTokenAddress)).value.uiAmount;
+        }
+
+        let quoteBalance = 0;
+        if((await connection.getBalance(quoteTokenAddress)) > 0) {
+          quoteBalance = (await connection.getTokenAccountBalance(quoteTokenAddress)).value.uiAmount;
+        }
+
+        console.log("base balance: ", baseBalance);
+        console.log("quote balance: ", quoteBalance);
+
+        setBaseTokenBalance(_ => baseBalance);
+        setQuoteTokenBalance(_ => quoteBalance);
+      }
+      else {
+        setBaseTokenBalance(_ => 0);
+        setQuoteTokenBalance(_ => 0);
+      }
+    }
+
+    updateBalance();
+  }, [walletState]);
 
   // Handle change for minimum price
   const handleMinPriceChange = (e) => {
@@ -94,6 +127,20 @@ const OrderProducer = ({
     }
   };
 
+  const removeCommas = (value) => {
+    return value.replace(/,/g, "");
+  };
+
+  const formatWithCommas = (value) => {
+    const withoutCommas = removeCommas(value);
+
+    const parts = withoutCommas.split(".");
+    const integerPart = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    const formattedValue =
+      parts.length > 1 ? `${integerPart}.${parts[1]}` : integerPart;
+    return formattedValue;
+  };
+
   return (
     <div className={styles.orderProducerContainer}>
       <div className={styles.formHeaderContainer}>
@@ -120,6 +167,7 @@ const OrderProducer = ({
                         color: "#ddd",
                         border: "none",
                         caretColor: "#ddd",
+                        padding: "1rem"
                       }}
                       min="0"
                       step="0.01" // Allow any decimal value
@@ -145,6 +193,7 @@ const OrderProducer = ({
                         color: "#ddd",
                         border: "none",
                         caretColor: "#ddd",
+                        padding: "1rem"
                       }}
                       min="0"
                       step="0.01" // Allow any decimal value
@@ -170,6 +219,7 @@ const OrderProducer = ({
                         color: "#ddd",
                         border: "none",
                         caretColor: "#ddd",
+                        padding: "1rem"
                       }}
                       min="0"
                       step="0.01" // Allow any decimal value
@@ -180,7 +230,7 @@ const OrderProducer = ({
                   </div>
                 </Form.Group>
                 <Form.Group controlId="formInput" className={styles.formGroupContainer}>
-                  <div className={styles.formLabelAndFieldContainer}>
+                  <div className={styles.formLabelAndFieldContainerNoBottomMargin}>
                     <Form.Label className={styles.formLabelContainer}>
                       <span>Size</span>
                     </Form.Label>
@@ -195,6 +245,7 @@ const OrderProducer = ({
                         color: "#ddd",
                         border: "none",
                         caretColor: "#ddd",
+                        padding: "1rem"
                       }}
                       min="0"
                       step="0.01" // Allow any decimal value
@@ -202,6 +253,21 @@ const OrderProducer = ({
                       onChange={(e) => handleAmountQuoteChange(e)}
                       value={amountQuote} // Use inputText instead of inputAmount to show the decimal value
                     />
+                  </div>
+                  <div className={styles.tokenBalanceContainer}>
+                    {walletState.connected ? (
+                      <div className={styles.userBalanceContainer}>
+                        <span>
+                          <i className="fa-solid fa-wallet fa-2xs"></i>
+                          {` `}
+                          {`${quoteTokenBalance}`}
+                        </span>
+                      </div>
+                    ) : (
+                      <div className={styles.userBalanceContainer}>
+                        <span>{` `}</span>
+                      </div>
+                    )}
                   </div>
                 </Form.Group>
                 <Form.Group controlId="formInput" className={styles.formGroupContainer}>
