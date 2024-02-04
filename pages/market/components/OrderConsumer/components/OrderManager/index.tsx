509369cd1570fc7249f4c8a019d1bcb0521cacee
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-02-04 16:56:36 +0530

    FUTUREUSE - Remove orderManager filters for now

--- pages/market/components/OrderConsumer/components/OrderManager/index.tsx
+++ pages/market/components/OrderConsumer/components/OrderManager/index.tsx
@@ -24,18 +24,8 @@ const OrderManager = ({
     phoenixClient
 }: OrderManagerProps) => {
 
-    const [allMarketsSelector, setAllMarketsSelector] = useState<boolean>(true);
-    const [orderStatusFilter, setOrderStatusFilter] = useState<OrderStatus>(OrderStatus.All);
-
-    const [allMarketsDropdownOpen, setAllMarketsDropdownOpen] = useState(false);
-    const [orderStatusDropdownOpen, setOrderStatusDropdownOpen] = useState(false);
     const [isCancelAllActionActive, setIsCancelAllActionActive] = useState(false);
     let [activeOrdersForTrader, setActiveOrdersForTrader] = useState<Order[]>([]);
-
-    const allMarketsDropdownRef = useRef(null);
-    const orderStatusDropdownRef = useRef(null);
-
-    let allOrderStatusFilters = getAllOrderStatus();
 
     const walletState = useWallet();
     let connection: Connection;
@@ -45,12 +35,6 @@ const OrderManager = ({
     else {
         connection = new Connection(`https://api.mainnet-beta.solana.com/`, { commitment: "processed" });
     }
-
-    const handleOrderStatusFilterUpdate = (
-        newOrderStatusFilter: OrderStatus
-    ) => {
-        setOrderStatusFilter(_ => newOrderStatusFilter);
-    };
 
     const handleCancelAllAction = async () => {
         setIsCancelAllActionActive(true)
@@ -141,24 +125,6 @@ const OrderManager = ({
     }
 
     useEffect(() => {
-        const handleClickOutside = (event) => {
-          if (allMarketsDropdownRef.current && !allMarketsDropdownRef.current.contains(event.target)) {
-            setAllMarketsDropdownOpen(false);
-          }
-
-          if (orderStatusDropdownRef.current && !orderStatusDropdownRef.current.contains(event.target)) {
-            setOrderStatusDropdownOpen(false);
-          }
-        };
-    
-        document.addEventListener('mousedown', handleClickOutside);
-    
-        return () => {
-          document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, []);
-
-    useEffect(() => {
         const refreshActiveOrdersForTrader = async () => {
             if(walletState.connected && enumeratedMarket.spotGridMarket) {
                 let orders: Order[] = [];
@@ -220,116 +186,6 @@ const OrderManager = ({
             <div className={styles.topMenuContainer}>
                 <span className={styles.orderTitleContainer}>Orders</span>
                 <div className={styles.topMenuButtonContainer}>
-                    {/* <div className={styles.allMarketsDropdownContainer} ref={allMarketsDropdownRef}>
-                        <div
-                            className={styles.allMarketsDropdownButtonContainer}
-                            onClick={() => {
-                                setAllMarketsDropdownOpen(_ => !allMarketsDropdownOpen);
-                            }}
-                        >
-                            <button className={styles.allMarketsDropdownButton}>
-                                <div className={styles.allMarketsDropdownInnerContainer}>
-                                    <div className={styles.allMarketsDropdownText}>
-                                        {
-                                            allMarketsSelector ?
-                                                <span >All markets</span>
-                                            :
-                                                <span>{baseTokenMetadata && quoteTokenMetadata ? `${baseTokenMetadata.ticker} - ${quoteTokenMetadata.ticker}` : `This`}</span>
-                                        }
-                                    </div>
-                                    <div className={styles.caretContainer}>
-                                        {allMarketsDropdownOpen ? (
-                                        <i className="fa-solid fa-caret-up"></i>
-                                        ) : (
-                                        <i className="fa-solid fa-caret-down"></i>
-                                        )}
-                                    </div>
-                                </div>
-                            </button>
-                        </div>
-                        <div className={styles.allMarketsDropdownContent}>
-                            {
-                                allMarketsDropdownOpen ?
-                                    <div>
-                                        {
-                                            <button className={styles.allMarketsDropdownButtonSecondary} onClick={
-                                                () => {
-                                                    setAllMarketsDropdownOpen(_ => !allMarketsDropdownOpen);
-                                                    setAllMarketsSelector(_ => !allMarketsSelector);
-                                                }
-                                            }>
-                                                <div className={styles.allMarketsDropdownInnerContainer}>
-                                                    {
-                                                        allMarketsSelector ?
-                                                            <span>{baseTokenMetadata && quoteTokenMetadata ? `${baseTokenMetadata.ticker} - ${quoteTokenMetadata.ticker}` : `This`}</span>
-                                                        :
-                                                            <span>All markets</span>
-                                                    }
-                                                </div>
-                                            </button>
-                                        }
-                                    </div>
-                                :
-                                    <></>
-                            }
-                        </div>
-                    </div> */}
-                    {/* <div className={styles.orderStatusDropdownContainer} ref={orderStatusDropdownRef}>
-                        <div
-                            className={styles.orderStatusDropdownButtonContainer}
-                            onClick={
-                                () => {
-                                    setOrderStatusDropdownOpen(_ => !orderStatusDropdownOpen)
-                                }
-                            }
-                        >
-                            <button className={styles.orderStatusDropdownButton}>
-                                <div className={styles.orderStatusDropdownInnerContainer}>
-                                    <div className={styles.orderStatusDropdownText}>
-                                        <span>
-                                            {
-                                                getOrderStatusText(orderStatusFilter)
-                                            }
-                                        </span>
-                                    </div>
-                                    <div className={styles.caretContainer}>
-                                        {orderStatusDropdownOpen ? (
-                                        <i className="fa-solid fa-caret-up"></i>
-                                        ) : (
-                                        <i className="fa-solid fa-caret-down"></i>
-                                        )}
-                                    </div>
-                                </div>
-                            </button>
-                        </div>
-                        <div className={styles.orderStatusDropdownContent}>
-                        {
-                            orderStatusDropdownOpen ?
-                            <div className={styles.orderStatusDropdownButtonSecondaryContainer}>
-                                {
-                                    allOrderStatusFilters.map((status) => {
-                                        if(status !== orderStatusFilter) {
-                                            return (
-                                                <button key = {status.toString()} className={styles.orderStatusDropdownButtonSecondary} onClick={
-                                                () => {
-                                                    setOrderStatusDropdownOpen(_ => !orderStatusDropdownOpen);
-                                                    handleOrderStatusFilterUpdate(status)
-                                                }
-                                                }>
-                                                <div className={styles.orderStatusDropdownInnerContainer}>
-                                                    <span>{getOrderStatusText(status)}</span>
-                                                </div>
-                                                </button>
-                                            )
-                                        }
-                                    })
-                                }
-                            </div>
-                            :
-                            <></>
-                        }
-                        </div>
-                    </div> */}
                     <div
                         className={styles.cancelAllButtonContainer}
                         onClick={ () => {
