922775839eee1b0e95a5d95a21adc506348d562a
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-02-05 15:02:50 +0530

    Use Inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -63,6 +63,10 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         crossOrigin="anonymous"
         referrerPolicy="no-referrer"
       />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
     </>
   );
 };
