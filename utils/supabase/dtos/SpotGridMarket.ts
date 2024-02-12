491d03d3247fa191949742d374b77acc60fccb60
Dhrumil Mayur Mehta <mehtadhrumil97@gmail.com>
2024-02-12 15:59:25 +0530

    Remove unused cols from SpotGridMaret

--- utils/supabase/dtos/SpotGridMarket.ts
+++ utils/supabase/dtos/SpotGridMarket.ts
@@ -1,13 +1,7 @@
 export interface SpotGridMarket {
-  spot_grid_market_address: string;
   phoenix_market_address: string;
-  spot_grid_market_key: string;
-  owner: string;
   base_token_mint: string;
   quote_token_mint: string;
-  withdrawal_fee_in_bps_hundredths: string;
-  min_order_spacing_in_ticks: string;
-  min_order_size_in_base_lots: string;
   taker_fee_bps: string;
   tick_size: string;
 }
